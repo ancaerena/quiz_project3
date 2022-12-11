2b128d1334c76ed9c3dd81e2217784127c1c097f
ancaerena <anca.erena@yahoo.ro>
2022-12-11 18:18:23 +0000

    Add options to answers for a valid answer and add lowercase to options

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Bugs:
 User's answer was in the same line as the question. Resolved by adding \n at the end of the question.
 Printing the delimitation symbol ~ gave an error: SyntaxError: invalid syntax. Fixed it by adding quotation marks around the symbols to make it a string
+
+StockFlow - in finding the string to import for lowercase enumeration
--- run.py
+++ run.py
@@ -1,50 +1,138 @@
+from string import ascii_lowercase
 def welcome_message():
+    """
+    Welcoming message with instructions and name input
+    """
     print("~~~~~~~~~~~~~~")
-    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favorite TV show is tested!")
-    print("Choose the right answer and find out your score at the end")
+    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favorite TV show is tested in 30 questions!")
+    print("Choose the right answer from the 4 available options and find out your score at the end! Only one answer is correct!")
     print("~~~~~~~~~~~~~~")
-    name = input("Enter your name to start the game:\n")
+    
 welcome_message()
+
+name = input("Enter your name to start the game:\n")
 
 """
 Add the structure for questions and answers. Add lists and tuples to store all questions and answers so the code isn't repetitive
+Added multiple choices for valid correct answer
 """
-QUESTIONS = [
-    ("How many seasons F.R.I.E.N.D.S TV show has", "10"),
-    ("What is the name of Phoebe's twin sister", "Ursula"),
-    ("Who was the previous tendent of Ross's new appartment", "Ugly naked guy"),
-    ("What was the occupation of Rachel's fiancé Barry Farber", "Orthodontist"),
-    ("In which season Ross and Rachel get together for the first time", "2"),
-    ("What is Chandler Bing's middle name", "Muriel"),
-    ("What was the profession of Joey's imaginary friend", "Space cowboy"),
-    ("Which of the Friends characters broke a swing as a child", "Monica"),
-    ("What piece was missing from Rachel's plane when leaving for Paris", "Left phalange"),
-    ("What year does Ross imply Rachel hasn't cooked since in 'The one with Phoebe's Birthday Dinner'", "1996"),
-    ("How many times does Ross get divorced", "3"),
-    ("Who mistakingly threw a woman's wooden leg into a fire", "Joey"),
-    ("In which season does Phoebe get married", "10"),
-    ("What was the name of Ross' pet monkey", "Marcel"),
-    ("What do you need to have in order to always be prepared for an attack", "Unagi"),
-    ("How many characters wore a raw turkey on their head", "2"),
-    ("What are the names of Rachel's sisters", "Jill and Amy"),
-    ("What is the only appropiate word to use when moving a couch", "Pivot"),
-    ("Who was Rachel's prom date", "Chip Matthews"),
-    ("How many pages was Rachel's letter to Ross", "18 pages"),
-    ("In an effort to get over Richard, Monica started making what", "Jam"),
-    ("What was Joey's fake name", "Ken Adams"),
-    ("Phoebe thought 'Kenny the Copy Guy' was who", "Ralph Lauren"),
-    ("Monica could not tell time until what age", "13"),
-    ("Who is the youngest Friend", "Rachel"),
-    ("Which of Monica's boyfriends wanted to become the Ultimate Fighting Champion", "Pete"),
-    ("Ross and Rachel's daughter Emma would have turned 18 what year", "2020"),
-    ("What kind of bed did the Mattress King deliver to Monica", "A racecar bed"),
-    ("How much the ticket to Yemen cost", "2100 dolars"),
-    ("Joey was cast as the butt double for which actor","Al Pacino")
-]
+QUESTIONS = {
+    "How many seasons F.R.I.E.N.D.S TV show has": [
+        "10", "8", "6", "12",
+    ],
+    "What is the name of Phoebe's twin sister": [
+        "Ursula",
+        "Muriel",
+        "Una",
+        "Angela",
+    ],
+    "Who was the previous tendent of Ross's new appartment":[
+        "Ugly naked guy",
+        "Cute dressed guy",
+        "The Fat neighbour",
+        "Mike",
+    ],
+    "What was the occupation of Rachel's fiancé Barry Farber":[
+        "Orthodontist",
+        "Doctor",
+        "Dentist",
+        "Gynecologist",
+    ],
+    "In which season Ross and Rachel get together for the first time": [
+        "2", "8", "1", "3",
+    ],
+    "What is Chandler Bing's middle name":[
+        "Muriel", "Eric", "Mathew", "John",
+    ],
+    "What was the profession of Joey's imaginary friend": [
+        "Space cowboy", "Cowboy", "Penguin", "Actor",
+    ],
+    "Which of the Friends characters broke a swing as a child":[ 
+        "Monica", "Phoebe", "Janice", "Joey",
+    ],
+    "What piece was missing from Rachel's plane when leaving for Paris": [
+        "Left phalange", "A wing", "Left engine", "Right engine",
+    ],
+    "What year does Ross imply Rachel hasn't cooked since": [
+        "1996", "2022", "1990", "2001",
+    ],
+    "How many times does Ross get divorced": [
+        "3", "4", "1", "5",
+    ],
+    "Who mistakingly threw a woman's wooden leg into a fire": [
+        "Joey", "Ross", "Mike", "Phoebe",
+    ],
+    "In which season does Phoebe get married": [
+        "10", "She doesn't get married", "8", "9",
+    ],
+    "What was the name of Ross' pet monkey": [
+        "Marcel", "Monkey", "Monica", "Susan",
+    ],
+    "What do you need to have in order to always be prepared for an attack": [
+        "Unagi", "A knife", "Good lungs to scream", "A sword",
+    ],
+    "How many characters wore a raw turkey on their head": [
+        "2", "All 6", "1", "None",
+    ],
+    "What are the names of Rachel's sisters": [
+        "Jill and Amy", "Amy and Monica", "Amelia and Joleen", "Judy and Ana",
+    ],
+    "What is the only appropriate word to use when moving a couch": [
+        "Pivot", "Ready, steady, go", "Unagi", "Green means go",
+    ],
+    "Who was Rachel's prom date": [
+        "Chip Matthews", "Matthew Chips", "Ross Geller", "Monica Geller",
+    ],
+    "How many pages was Rachel's letter to Ross": [
+        "18 pages", "10 pages front and back", "12", "20",
+    ],
+    "In an effort to get over Richard, Monica started making what": [
+        "Jam", "Pancakes", "Pie", "Origami",
+    ],
+    "What was Joey's fake name": [
+        "Ken Adams", "Drake Ramoray", "Chandler Bing", "Troy Ribbianni",
+    ],
+    "Phoebe thought 'Kenny the Copy Guy' was who": [
+        "Ralph Lauren", "Bruce Willis", "Brad Pitt", "Her brother",
+    ],
+    "Monica could not tell time until what age": [
+        "13", "6", "20", "30",
+    ],
+    "Who is the youngest Friend": [
+        "Rachel", "Monica", "Phoebe", "Joey",
+    ],
+    "Which of Monica's boyfriends wanted to become a Fighting Champion": [
+        "Pete", "Chandler", "Richard", "Julio",
+    ],
+    "Ross and Rachel's daughter Emma would have turned 18 what year": [
+        "2020", "2010", "2000", "2030",
+    ],
+    "What kind of bed did the Mattress King deliver to Monica": [
+        "A racecar bed", "A princess bed", "A single bed", "A couch",
+    ],
+    "How much the ticket to Yemen cost": [
+        "2100 dolars", "300 dolars", "They were free", "1000 dolars",
+    ],
+    "Joey was cast as the butt double for which actor"[
+        "Al Pacino", "Matthew Perry", "George Clooney", "Alec Baldwin",
+    ]
+}
 
-for question, correct_answer in QUESTIONS:
-    answer = input(f"{question}?\n")
+""""
+for loop to iterate through questions and answers
+used sorted() to randomly display the answers
+"""
+for num, (question, options) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}: ")
+    print(f"{question}?\n")
+    correct_answer = options[0]
+    labeled_options = dict(zip(ascii_lowercase, sorted(options)))
+    for label, option in labeled_options.items():
+        print(f"  {label}) {options}")
+
+    answer_label = input("\n Your answer is:\n")
+    answer = labeled_options.get(answer_label)
     if answer == correct_answer:
-        print("Well done you! You sure know your friends!")
+        print(f"Well done, {name}! You sure know your friends!")
     else:
-        print(f"You've been bamboozled!The asnwer is {correct_answer!r}, not {answer!r}\n")
+        print(f"You've been bamboozled! The answer is {correct_answer!r}, not {answer!r}\n")
