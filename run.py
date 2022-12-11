552b9997aad41ed627b622c54f40659d774314c0
ancaerena <anca.erena@yahoo.ro>
2022-12-11 15:54:54 +0000

    Add the first question of the trivia

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 def welcome_message():
-    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favourile TV show is tested!")
+    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favourite TV show is tested!")
     print("Choose the right answer and find out your score at the end")
     name = input("Enter your name to start the game:\n")
 welcome_message()
 
+"""
+Add the structure for questions and answers
+"""
+answer = input("How many seasons did the F.R.I.E.N.D.S tv series had?\n")
+if answer == "10":
+    print("Correct, well done for your first question!")
+else:
+    print(f"The answer is '10' , not {answer!r}")
