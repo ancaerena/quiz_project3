e630b8aca8338ebc7d04006bc17c857dd34dba9d
ancaerena <anca.erena@yahoo.ro>
2022-12-11 16:48:47 +0000

    Add all 30 questions and spaces after each answer

--- run.py
+++ run.py
@@ -22,12 +22,29 @@ QUESTIONS = [
     ("What year does Ross imply Rachel hasn't cooked since in 'The one with Phoebe's Birthday Dinner'", "1996"),
     ("How many times does Ross get divorced", "3"),
     ("Who mistakingly threw a woman's wooden leg into a fire", "Joey"),
-    ("In which season does Phoebe get married", "10")
+    ("In which season does Phoebe get married", "10"),
+    ("What was the name of Ross' pet monkey", "Marcel"),
+    ("What do you need to have in order to always be prepared for an attack", "Unagi"),
+    ("How many characters wore a raw turkey on their head", "2"),
+    ("What are the names of Rachel's sisters", "Jill and Amy"),
+    ("What is the only appropiate word to use when moving a couch", "Pivot"),
+    ("Who was Rachel's prom date", "Chip Matthews"),
+    ("How many pages was Rachel's letter to Ross", "18 pages"),
+    ("In an effort to get over Richard, Monica started making what", "Jam"),
+    ("What was Joey's fake name", "Ken Adams"),
+    ("Phoebe thought 'Kenny the Copy Guy' was who", "Ralph Lauren"),
+    ("Monica could not tell time until what age", "13"),
+    ("Who is the youngest Friend", "Rachel"),
+    ("Which of Monica's boyfriends wanted to become the Ultimate Fighting Champion", "Pete"),
+    ("Ross and Rachel's daughter Emma would have turned 18 what year", "2020"),
+    ("What kind of bed did the Mattress King deliver to Monica", "A racecar bed"),
+    ("How much the ticket to Yemen cost", "2100 dolars"),
+    ("Joey was cast as the butt double for which actor","Al Pacino")
 ]
 
 for question, correct_answer in QUESTIONS:
-    answer = input(f"{question}? ")
+    answer = input(f"{question}?\n")
     if answer == correct_answer:
         print("Well done you! You sure know your friends!")
     else:
-        print(f"You've been bamboozled!The asnwer is {correct_answer!r}, not {answer!r}")
+        print(f"You've been bamboozled!The asnwer is {correct_answer!r}, not {answer!r}\n")
