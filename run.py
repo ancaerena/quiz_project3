a1bbdd9a6c1fd8d865a4ad1d78a8e04ca7acc2b5
ancaerena <anca.erena@yahoo.ro>
2022-12-11 15:43:01 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message():
+    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favourile TV show is tested!")
+    print("Choose the right answer and find out your score at the end")
+    name = input("Enter your name to start the game:\n")
+welcome_message()
+
