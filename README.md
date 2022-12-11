1c68a9fde18650e6930683637b7956834d81f067
ancaerena <anca.erena@yahoo.ro>
2022-12-11 16:26:25 +0000

    Add more questions and answers using lists and tuples

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Bugs:
 User's answer was in the same line as the question. Resolved by adding \n at the end of the question.
+Printing the delimitation symbol ~ gave an error: SyntaxError: invalid syntax. Fixed it by adding quotation marks around the symbols to make it a string
--- run.py
+++ run.py
@@ -1,14 +1,33 @@
 def welcome_message():
-    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favourite TV show is tested!")
+    print("~~~~~~~~~~~~~~")
+    print("welcome to F.R.I.E.N.D.S trivia, where your knowledge about your favorite TV show is tested!")
     print("Choose the right answer and find out your score at the end")
+    print("~~~~~~~~~~~~~~")
     name = input("Enter your name to start the game:\n")
 welcome_message()
 
 """
-Add the structure for questions and answers
+Add the structure for questions and answers. Add lists and tuples to store all questions and answers so the code isn't repetitive
 """
-answer = input("How many seasons did the F.R.I.E.N.D.S tv series had?\n")
-if answer == "10":
-    print("Correct, well done for your first question!")
-else:
-    print(f"The answer is '10' , not {answer!r}")
+QUESTIONS = [
+    ("How many seasons F.R.I.E.N.D.S TV show has", "10"),
+    ("What is the name of Phoebe's twin sister", "Ursula"),
+    ("Who was the previous tendent of Ross's new appartment", "Ugly naked guy"),
+    ("What was the occupation of Rachel's fiancé Barry Farber", "Orthodontist"),
+    ("In which season Ross and Rachel get together for the first time", "2"),
+    ("What is Chandler Bing's middle name", "Muriel"),
+    ("What was the profession of Joey's imaginary friend", "Space cowboy"),
+    ("Which of the Friends characters broke a swing as a child", "Monica"),
+    ("What piece was missing from Rachel's plane when leaving for Paris", "Left phalange"),
+    ("What year does Ross imply Rachel hasn't cooked since in 'The one with Phoebe's Birthday Dinner'", "1996"),
+    ("How many times does Ross get divorced", "3"),
+    ("Who mistakingly threw a woman's wooden leg into a fire", "Joey"),
+    ("In which season does Phoebe get married", "10")
+]
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Well done you! You sure know your friends!")
+    else:
+        print(f"You've been bamboozled!The asnwer is {correct_answer!r}, not {answer!r}")
