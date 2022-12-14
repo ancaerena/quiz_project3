4a15086cedfb57e3f3d1e81eed973b32159a6358
ancaerena <anca.erena@yahoo.ro>
2022-12-14 15:07:55 +0000

    Organise code in functions and add main function

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ User's answer was in the same line as the question. Resolved by adding \n at the
 Printing the delimitation symbol ~ gave an error: SyntaxError: invalid syntax. Fixed it by adding quotation marks around the symbols to make it a string
 Error running run.py because of an indentation error. Found that ":" was missing from the last question before the list of answers
 Answers were showing in lists for each lowercase letter enumeration. The issue was a typo in print statement for printing the answers, instead of {option} it was {options}
+Question to add the name after some questions to continue the game - I was calling the add_name function in the choices feedback
+Validate user answer if using uppercase - using lower() have transformed the answer to lowercase
+
 
 
 StockFlow - in finding the string to import for lowercase enumeration
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 from string import ascii_lowercase
 
 
@@ -12,15 +13,19 @@ def welcome_message():
     print("~~~~~~~~~~~~~~")
 
 
-welcome_message()
-
-name = input("Enter your name to start the game:\n")
+def add_name():
+     while True:
+        name = input("Enter your name to start the game:\n")
+        if name.strip() != '':
+            break
+    
 
 """
 Add the structure for questions and answers
 Add dictionary to store all questions and answers so the code isn't repetitive
 Added multiple choices for valid correct answer
 """
+TOTAL_QUESTIONS = 10
 QUESTIONS = {
     "What piece was missing from Rachel's plane when leaving for Paris": [
         "Left phalange", "A wing", "Left engine", "Right engine",
@@ -37,7 +42,7 @@ QUESTIONS = {
     "Who was Rachel's prom date": [
         "Chip Matthews", "Matthew Chips", "Ross Geller", "Monica Geller",
     ],
-    "What was Joey's fake name": [
+   "What was Joey's fake name": [
         "Ken Adams", "Drake Ramoray", "Chandler Bing", "Troy Ribbianni",
     ],
     "Phoebe thought 'Kenny the Copy Guy' was who": [
@@ -54,40 +59,58 @@ QUESTIONS = {
     ]
 }
 
-"""
-Adding counting of correct answers
-"""
-num_correct = 0
-"""
-for loop to iterate through questions and answers
-used sorted() to randomly display the answers
-"""
 
-for num, (question, options) in enumerate(QUESTIONS.items(), start=1):
-    print(f"\nQuestion {num}: ")
+def organise_questions(questions, num_questions):
+    num_questions = min(num_questions, len(questions))
+    return random.sample(list(questions.items()), k=num_questions)
+
+
+def user_choice(question, options):
     print(f"{question}?\n")
-
-    correct_answer = options[0]
-
-    labeled_options = dict(zip(ascii_lowercase, sorted(options)))
-
+    labeled_options = dict(zip(ascii_lowercase, options))
     for label, option in labeled_options.items():
         print(f"  {label}) {option}")
-    """
-    Use a while loop to check the validity of inputed data
-    """
-    while (answer_label := input("\n Your answer is:\n")) not in labeled_options:
-        print(f"Please choose one of {', '.join(labeled_options)}")
-    answer = labeled_options[answer_label]
+
 
     """
-    If statement to check the correct answer
-    """
+    Use a while loop to check the validity of inputed data
+    """    
+    while (answer_label := input("\n Your answer is:\n").lower()) not in labeled_options:
+        print(f"Please choose one of {', '.join(labeled_options)}")
+    return labeled_options[answer_label]
+
+
+def show_questions(question, options):
+    correct_answer = options[0]
+    shuffled_options = random.sample(options, k=len(options))
+
+    answer = user_choice(question, shuffled_options)
     if answer == correct_answer:
-        num_correct += 1
-        print(f"Well done, {name}! You sure know your friends!")
+        print(f"Well done! You sure know your friends!")
+        return True
     else:
         print("You've been bamboozled!")
         print(f"The answer is {correct_answer!r}, not {answer!r}\n")
-        
-print(f"\n You got {num_correct} correct out of 10")
+        return False
+
+
+def start_quiz():
+    questions = organise_questions(QUESTIONS, TOTAL_QUESTIONS)
+
+    num_correct = 0
+    """
+    for loop to iterate through questions and answers
+    used sorted() to randomly display the answers
+    """
+    for num, (question, options) in enumerate(questions, start=1):
+        print(f"\nQuestion {num}: ")
+        num_correct += show_questions(question, options)
+    print(f"\n You got {num_correct} correct out of {num}")
+      
+
+def main():
+    welcome_message()
+    add_name()
+    start_quiz()
+
+main()
